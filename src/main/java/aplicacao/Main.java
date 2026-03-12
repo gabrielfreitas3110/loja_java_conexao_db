13aeb94daa0d56d98feb96bbc0dab4f30a2278b6
Gabriel Augusto <gabriel.freitas3110@gmail.com>
2026-03-11 21:47:55 -0300

    feat: update, delete e busca personalizada

--- src/main/java/aplicacao/Main.java
+++ src/main/java/aplicacao/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ProdutoDAO produtoDAO = new ProdutoDAO();
 
+        /*
         produtoDAO.inserir("Mouse", "Periférico", 150.0, 10);
         produtoDAO.inserir("Teclado", "Periférico", 200.0, 20);
         produtoDAO.inserir("Headset", "Periférico", 120.0, 30);
@@ -17,6 +18,32 @@ public class Main {
 
         for(Produto produto : produtos) {
             System.out.println(produto.toString());
+        }
+
+        Produto produto = new Produto();
+        produto.setId(19L);
+        produto.setNome("Monitor");
+        produto.setCategoria("Periférico");
+        produto.setPreco(2100.00);
+        produto.setEstoque(5);
+        produtoDAO.atualizarProduto(produto);
+
+        produtos = produtoDAO.buscarProdutos();
+
+        for(Produto p : produtos) {
+            System.out.println(p.toString());
+        }
+
+        produtoDAO.deletarProduto(13L);
+        produtoDAO.deletarProduto(16L);
+        produtoDAO.deletarProduto(20L);
+
+
+         */
+        List<Produto> produtos = produtoDAO.buscaPersonalizadaProdutos("ead");
+
+        for(Produto p : produtos) {
+            System.out.println(p.toString());
         }
     }
 }
--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -62,4 +62,74 @@ public class ProdutoDAO {
 
         return produtos;
     }
+
+    public void atualizarProduto(Produto produto) {
+        String sql = "UPDATE produto SET nome = ?, categoria = ?, preco = ?, estoque = ? " +
+                "WHERE id = ? ";
+
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setString(2, produto.getCategoria());
+            stmt.setDouble(3, produto.getPreco());
+            stmt.setInt(4, produto.getEstoque());
+            stmt.setLong(5, produto.getId());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deletarProduto(Long id) {
+        String sql = "DELETE FROM produto WHERE id = ?";
+
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setLong(1, id);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Produto> buscaPersonalizadaProdutos(String nome) {
+        String sql = "SELECT * FROM produto WHERE nome LIKE ? ";
+        Connection connection = ConnectionFactory.getConnection();
+
+        List<Produto> produtos = new ArrayList<>(1);
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, "%" + nome + "%");
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+                Produto produto = new Produto();
+                produto.setId(rs.getLong("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setCategoria(rs.getString("categoria"));
+                produto.setPreco(rs.getDouble("preco"));
+                produto.setEstoque(rs.getInt("estoque"));
+
+                produtos.add(produto);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        ConnectionFactory.closeConnection(connection);
+
+        return produtos;
+    }
+
 }
